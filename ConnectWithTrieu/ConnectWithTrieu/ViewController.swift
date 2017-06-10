b913f97bc14fe91fbac94f04b4d1488725880699
Fayon Nova <hainguyenhoang0704@gmail.com>
2017-06-10 16:25:31 +0700

    Add label Add image on MainStoryBoard

--- ConnectWithTrieu/ConnectWithTrieu/ViewController.swift
+++ ConnectWithTrieu/ConnectWithTrieu/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var label: UILabel = {
+        var l = UILabel()
+        
+        return l
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
