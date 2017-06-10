19f5457f4336af635cd1d3d387d1898e9cc7e6ee
nguyendinhtrieu1996 <nguyendinhtrieu1996@gmail.com>
2017-06-10 16:24:29 +0700

    Add TestGitViewController - Developer: Trieu

---
+++ ConnectWithTrieu/ConnectWithTrieu/TestGitViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TestGitViewController.swift
+//  ConnectWithTrieu
+//
+//  Created by Os on 6/10/17.
+//  Copyright © 2017 HHVermillion. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TestGitViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
